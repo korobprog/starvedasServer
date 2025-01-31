dc0b3741ab583fb22b65c5524e6cb4d6bfb3cc1e
Maxim Korobkov <62425861+korobprog@users.noreply.github.com>
2025-01-31 14:53:11 +1000

    token

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,6 +3,7 @@ const { prisma } = require('../prisma/prisma-client');
 const bcrypt = require('bcryptjs'); // Исправлено имя переменной
 const path = require('path');
 const fs = require('fs');
+const jwt = require('jsonwebtoken');
 
 const UserController = {
   register: async (req, res) => {
@@ -56,7 +57,30 @@ const UserController = {
   },
 
   login: async (req, res) => {
-    res.send('login');
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ error: 'Все поля обязательны!' });
+    }
+
+    try {
+      const user = await prisma.user.findUnique({ where: { email } });
+      if (!user) {
+        return res.status(400).json({ error: 'Неверный логин или пароль' });
+      }
+
+      const valid = await bcrypt.compare(password, user.password);
+
+      if (!valid) {
+        return res.status(400).json({ error: 'Неверный логин или пароль' });
+      }
+
+      const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY);
+      res.json({ token });
+    } catch (error) {
+      console.error('Login error', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
   },
   getUserById: async (req, res) => {
     res.send('getUserById');
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const { UserController } = require('../controllers');
+const { authenticateToken } = require('../middleware/auth');
 const uploadDestination = 'uploads';
 
 // Показываем где хранить файлы
@@ -18,7 +19,7 @@ const uploads = multer({ storage: storage });
 // Пользователи роуты
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
-router.get('/current', UserController.current);
+router.get('/current', authenticateToken, UserController.current);
 router.get('/users/:id', UserController.getUserById);
 router.put('/users/:id', UserController.updateUser);
 
